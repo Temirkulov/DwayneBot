53d068f651436c1ec985bd013a7721199b069091
The Rock <mirkosht@gmail.com>
2020-12-02 19:01:36 +0800

    v1.0.0

--- index.js
+++ index.js
@@ -62,12 +62,74 @@ client.on('ready', () => {
             {name: "Help", value: "Lists out the help menu"},
             {name: "cc/clearchannel", value: "clears last 100 messages **(only for Admins)**"},
             {name: "status", value: "Change the bot's status"},
-            {name: "ping", value: "checks the bot's ping"}
+            {name: "ping", value: "checks the bot's ping"},
+            {name: "kick", value: "Kicks a specified user"},
+            {name: "ban", value: "bans a specified user"},
+            {name: "stats", value: "Displays the bot Statistics"}
 
         )
         message.channel.send(embed)
     })
+    command(client,'ban', (message) => {
+        const { member, mentions } = message
 
+        const tag = `<@${member.id}>`
+
+        if (member.hasPermission('ADMINISTRATOR') || 
+        member.hasPermission('BAN_MEMBERS')
+        ) {
+                const target = mentions.users.first()
+                if(target) {
+                   const targetMember = message.guild.members.cache.get(target.id)
+                   targetMember.ban()
+                   message.channel.send(`${tag} That user has been banned.`)
+                } else {
+                    message.channel.send(`${tag} Please specify someone to ban.`)
+                } 
+                
+            } else {
+                message.channel.send(
+                    `<${member.id}> You don't have permission to use this command.`
+                    )
+            }
+    })
+    command(client,'kick', (message) => {
+        const { member, mentions } = message
+
+        const tag = `<@${member.id}>`
+
+        if (member.hasPermission('ADMINISTRATOR') || 
+        member.hasPermission('KICK_MEMBERS')
+        ) {
+                const target = mentions.users.first()
+                if(target) {
+                   const targetMember = message.guild.members.cache.get(target.id)
+                   targetMember.kick()
+                   message.channel.send(`${tag} That user has been kicked.`)
+                } else {
+                    message.channel.send(`${tag} Please specify someone to kick.`)
+                } 
+                
+            } else {
+                message.channel.send(
+                    `<${member.id}> You don't have permission to use this command.`
+                    )
+            }
+    })
+    command(client,'c-ask',(message) => {
+        if(message.author.id === "348296915143884801") {
+            const channelIDs = `716745665643937862`
+            client.channels.cache.get(channelIDs).send(":taco: I'm feeling a little extra hungry today, could I buy \`1022`\ tacos? `Type 'sell' to sell them tacos!`")
+        }
+
+    })
+    command(client,'c-busy',(message) => {
+        if(message.author.id === "348296915143884801") {
+            const channelIDs = `716745665643937862`
+            client.channels.cache.get(channelIDs).send(":rage: Well it seems like everyone working here is lazy, I'm going to the fast food joint across the road.")
+        }
+
+    })
     
 })
 
